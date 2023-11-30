c0319e0204e03bcb573a2f95fc871f45c8ad6946
Daniel Sampar <daniel_sampar@hotmail.com>
2023-11-30 14:49:47 -0300

    first commit

---
+++ ui/src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { HeaderContainer, Logo, HeaderTitle } from './styles';
+
+class Header extends React.Component<{ logo: string }> {
+  state = {
+    showLogo: false,
+  };
+
+  componentDidUpdate(prevProps: any) {
+    // why this?
+    if (prevProps.logo !== this.props.logo) {
+      this.setState({ showLogo: true });
+    }
+  }
+
+  render() {
+    return (
+      <HeaderContainer>
+        {this.state.showLogo && <Logo src={this.props.logo} alt="logo" />}
+        <HeaderTitle>Travel Blog</HeaderTitle>
+      </HeaderContainer>
+    );
+  }
+}
+
+export default Header;
